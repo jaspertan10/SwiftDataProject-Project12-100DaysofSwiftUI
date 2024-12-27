797c207998a03091ef8d877b6611054c3e90cb98
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2024-12-26 22:46:14 -0800

    Iniital commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Jasper Tan on 12/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
